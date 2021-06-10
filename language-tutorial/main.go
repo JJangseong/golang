dbf9cbe5f30c0580adfe8499c6e3cb37ed9d75a5
Jangseongjin <sungjin5891@gmail.com>
2021-06-10 21:37:19 +0900

    Booleans & Conditionals

--- language-tutorial/main.go
+++ language-tutorial/main.go
@@ -105,10 +105,43 @@ func main() {
 	//for i := 0; i < 5; i++ {
 	//	fmt.Println("value of i is :", i)
 	//}
+	//
+	//names := []string{"yoshi", "mario", "peach", "bowser"}
+	//
+	//for _, v := range names {
+	//	fmt.Println(v)
+	//}
 
+	// Booleans & Conditionals
+
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
+	}
 	names := []string{"yoshi", "mario", "peach", "bowser"}
 
-	for _, v := range names {
-		fmt.Println(v)
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos ", index)
+			continue
+		}
+
+		if index > 2 {
+			fmt.Println("breaking at pos ", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
 	}
+
 }

c1bbf158264dde4c9d4892116f0bba6136cb00b8
Jangseongjin <sungjin5891@gmail.com>
2021-06-23 19:14:48 +0900

    Saving Files

--- language-tutorial/main.go
+++ language-tutorial/main.go
@@ -317,7 +317,8 @@ func promptOptions(b bill) {
 		b.updateTip(t)
 		promptOptions(b)
 	case "s":
-		fmt.Println("you chose to save the bill", b)
+		b.save()
+		fmt.Println("you saved the file - ", b.name)
 	default:
 		fmt.Println("that was not a valid option...")
 		promptOptions(b)
